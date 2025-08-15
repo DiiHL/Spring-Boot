08922eb10cf8d8ce288736e073a66cee62a45be4
Diego <diegoo.pbe@gmail.com>
2025-08-15 14:13:06 -0300

    First Steps in Java with Spring Boot

--- first-spring-boot/src/main/java/br/com/diih/controllers/GreetingController.java
+++ first-spring-boot/src/main/java/br/com/diih/controllers/GreetingController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
-@RequestMapping("/control")
 public class GreetingController {
 
     private static final String template = "Hello, %s!";
