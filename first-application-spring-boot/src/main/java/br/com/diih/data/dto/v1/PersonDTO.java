f81b05b32b380980156758b52e76ddefd69a45ee
Diego <diegoo.pbe@gmail.com>
2025-08-26 23:38:09 -0300

    feat: Add return YAML

--- first-application-spring-boot/src/main/java/br/com/diih/data/dto/v1/PersonDTO.java
+++ first-application-spring-boot/src/main/java/br/com/diih/data/dto/v1/PersonDTO.java
@@ -1,40 +1,33 @@
 package br.com.diih.data.dto.v1;
 
-//import com.fasterxml.jackson.annotation.JsonProperty;
-//import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-
 import br.com.diih.serializer.genderSerializer;
 import com.fasterxml.jackson.annotation.JsonFilter;
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
 
 import java.io.Serial;
 import java.io.Serializable;
-import java.util.Date;
 import java.util.Objects;
 
-//@JsonPropertyOrder({"id", "firstName", "lastName", "address", "gender"})
-@JsonFilter("PersonFilter")
+@JacksonXmlRootElement(localName = "PersonDTO")
+@JsonPropertyOrder({"id", "first_name", "last_name", "address", "gender"})
+//@JsonFilter("PersonFilter")
 public class PersonDTO implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
     private Long id;
 
-//    @JsonProperty("first_name")
+    @JsonProperty("first_name")
     private String firstName;
-    //@JsonProperty("last_name")
-    @JsonInclude(JsonInclude.Include.NON_NULL)
-    private String lastName;
-    @JsonInclude(JsonInclude.Include.NON_EMPTY)
-    private String phoneNumber;
-    @JsonFormat(pattern = "dd/MM/yyyy")
-    private Date birthDay;
-    private String address;
-    @JsonSerialize(using = genderSerializer.class)
-    private String gender;
 
-    private String sensitiveData;
+    @JsonProperty("last_name")
+    private String lastName;
+
+    private String address;
+//    @JsonSerialize(using = genderSerializer.class)
+    private String gender;
 
     public PersonDTO() {
     }
@@ -75,30 +68,6 @@ public class PersonDTO implements Serializable {
         return gender;
     }
 
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getSensitiveData() {
-        return sensitiveData;
-    }
-
-    public void setSensitiveData(String sensitiveData) {
-        this.sensitiveData = sensitiveData;
-    }
-
-    public Date getBirthDay() {
-        return birthDay;
-    }
-
-    public void setBirthDay(Date birthDay) {
-        this.birthDay = birthDay;
-    }
-
     public void setGender(String gender) {
         this.gender = gender;
     }
@@ -107,11 +76,12 @@ public class PersonDTO implements Serializable {
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
         PersonDTO personDTO = (PersonDTO) o;
-        return Objects.equals(getId(), personDTO.getId()) && Objects.equals(getFirstName(), personDTO.getFirstName()) && Objects.equals(getLastName(), personDTO.getLastName()) && Objects.equals(getPhoneNumber(), personDTO.getPhoneNumber()) && Objects.equals(getBirthDay(), personDTO.getBirthDay()) && Objects.equals(getAddress(), personDTO.getAddress()) && Objects.equals(getGender(), personDTO.getGender()) && Objects.equals(sensitiveData, personDTO.sensitiveData);
+        return Objects.equals(getId(), personDTO.getId()) && Objects.equals(getFirstName(), personDTO.getFirstName()) && Objects.equals(getLastName(), personDTO.getLastName()) && Objects.equals(getAddress(), personDTO.getAddress()) && Objects.equals(getGender(), personDTO.getGender());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(getId(), getFirstName(), getLastName(), getPhoneNumber(), getBirthDay(), getAddress(), getGender(), sensitiveData);
+        return Objects.hash(getId(), getFirstName(), getLastName(), getAddress(), getGender());
     }
 }
+
