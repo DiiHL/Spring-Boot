a815f6746864ee32122b869b6a35fbff213390c0
Diego <diegoo.pbe@gmail.com>
2026-01-25 15:03:45 -0300

    feat: adding CORS integration tests

--- StartingCORS/src/test/java/br/com/diih/config/TestConfigs.java
+++ StartingCORS/src/test/java/br/com/diih/config/TestConfigs.java
@@ -1,6 +1,11 @@
 package br.com.diih.config;
 
 public interface TestConfigs {
-    
+
     int SERVER_PORT = 8888;
+    String HEADER_PARAM_AUTHORIZATION = "Authorization";
+    String HEADER_PARAM_ORIGIN = "Origin";
+
+    String ORIGIN_LOCALHOST = "https://localhost:3000";
+    String ORIGIN_ERROR = "https://localhost:3333";
 }
